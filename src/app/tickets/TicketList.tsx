678cc91c4d631ddb2793ff7fc33f8b4234132da4
hayzedd2 <azeezalhameen1@gmail.com>
2024-02-01 12:23:07 +0100

    create new ticket

--- src/app/tickets/TicketList.tsx
+++ src/app/tickets/TicketList.tsx
@@ -10,7 +10,6 @@ interface TicketProps {
 }
 
 const getTickets = async () => {
-  await new Promise(resolve => setTimeout(resolve , 3000))
   const res = await fetch('http://localhost:4000/tickets' , {
     next : {
       revalidate : 0
--- src/app/tickets/create/CreateForm.tsx
+++ src/app/tickets/create/CreateForm.tsx
@@ -1,16 +1,35 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
 const CreateForm = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [priority, setPriority] = useState("low");
-  const [loading, isLoading] = useState(false);
+  const [loading, isLoading] = useState<boolean>(false);
   const router = useRouter();
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    const dataForm = {
+      title,
+      body,
+      priority,
+      user_email: "azeez@gmail.com",
+    };
+
+    const res = await fetch("http://localhost:4000/tickets", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(dataForm),
+    });
+    if (res.status === 201) {
+      router.refresh();
+      router.push("/tickets");
+    }
+  };
   return (
-    <form className="w-full">
+    <form className="w-full" onSubmit={handleSubmit}>
       <label>
         <span>Title :</span>
         <input
@@ -36,13 +55,15 @@ const CreateForm = () => {
           onChange={(e) => setPriority(e.target.value)}
           value={priority}
         >
-            <option value="low">Low Priority</option>
-            <option value="medium">Medium Priority</option>
-            <option value="high">High Priority</option>
+          <option value="low">Low Priority</option>
+          <option value="medium">Medium Priority</option>
+          <option value="high">High Priority</option>
         </select>
       </label>
+      <button className="btn btn-primary" disabled={loading}>
+        {loading ? <p>Adding ....</p> : <p>Add Ticket</p>}
+      </button>
     </form>
   );
 };
-
 export default CreateForm;
