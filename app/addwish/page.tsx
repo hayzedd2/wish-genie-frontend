68afdfae10ae62ba075e34abad603f0638cf09b6
hayzedd2 <azeezalhameen1@gmail.com>
2024-02-14 17:52:14 +0100

    add-wish ui

--- app/addwish/page.tsx
+++ app/addwish/page.tsx
@@ -9,10 +9,18 @@ import {
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { zodResolver } from "@hookform/resolvers/zod";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { MdOutlineVideoLibrary } from "react-icons/md";
 import * as z from "zod";
+import {
+  Select,
+  SelectValue,
+  SelectTrigger,
+  SelectContent,
+  SelectItem,
+} from "@/components/ui/select";
+import { Button } from "@/components/ui/button";
 
 const wishCategories = [
   {
@@ -55,7 +63,6 @@ const AddWish = () => {
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     // onsubmit function
   };
-
   return (
     <section className="pb-8 px-5">
       <div className="container max-w-xl mx-auto">
@@ -92,28 +99,56 @@ const AddWish = () => {
                 </FormItem>
               )}
             />
-            <div className="pills-container flex gap-3">
-              {wishCategories.map((wish) => (
-                <div
-                  className="bg-[#1C1C25] text-white shadow text-[0.95rem] px-3 py-2 rounded-md cursor-pointer"
-                  id={wish.id}
-                >
-                  {wish.catName}
-                </div>
-              ))}
-            </div>
+            <FormField
+              control={form.control}
+              name="wish_category"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Category</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl className="bg-[#1C1C25]">
+                      <SelectTrigger className="w-[100%]">
+                        <SelectValue placeholder="Choose a category that best suits your wish" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      {wishCategories.map((wish, index) => (
+                        <SelectItem value={wish.id} key={index}>
+                          {wish.catName}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                </FormItem>
+              )}
+            />
+
             <div className="text-[#9E9EB8] bg-[#1C1C25] w-full cursor-pointer rounded-md outline-none border-2 border-[#3d3d54] px-5 py-5 text-[1.05rem]">
-              <MdOutlineVideoLibrary className="text-white text-[1.35rem]" />
-              <div className="text mt-3">
-                <h2 className="text-white text-[0.95rem] font-[500]">
-                  Optional: Add a Video
-                </h2>
-                <p className="text-[0.9rem] mt-[0.15rem]">
-                  Upload a video to tell your story. (Max 2 mins)
-                </p>
-              </div>
-              {/* <input type="file" name="" id="" /> */}
+              <label
+                htmlFor="file"
+                className="text-white text-[0.9rem] mt-3 cursor-pointer"
+              >
+                <MdOutlineVideoLibrary className="text-white text-[1.35rem]" />
+                <div className="text mt-3">
+                  <h2 className="text-white text-[0.95rem] font-[500]">
+                    Optional: Add a Video
+                  </h2>
+                  <p className="text-[0.9rem] mt-[0.15rem]">
+                    Upload a video to tell your story. (Max 2 mins)
+                  </p>
+                </div>
+                <input type="file" name="file" id="file" className="hidden" />
+              </label>
             </div>
+
+           <div className="flex items-end justify-end">
+           <Button variant={"primary"}>
+              Submit
+            </Button>
+           </div>
           </form>
         </Form>
       </div>
--- app/components/ui/label.tsx
+++ app/components/ui/label.tsx
@@ -7,7 +7,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const labelVariants = cva(
-  "text-base text-[#E2E4E8] font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 my-2 "
+  "text-base text-[#E2E4E8] font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 my-0 "
 )
 
 const Label = React.forwardRef<
