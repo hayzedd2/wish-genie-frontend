76ac1cf05171eede378f0fc2c89ebbc972c0cca9
hayzedd2 <azeezalhameen1@gmail.com>
2024-03-13 14:58:07 +0100

    show data on frontend

--- app/wishes/FetchWishes.tsx
+++ app/wishes/FetchWishes.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { auth } from "@clerk/nextjs";
+import { useUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import prismadb from "@/lib/prismadb";
 import { Button } from "@/components/ui/button";
 const FetchWishes = async () => {
   const { userId } = auth();
+  //   const {isSignedIn , user} = useUser()
   if (!userId) {
     redirect("/sign-in");
   }
+  //   if(!isSignedIn){
+  //     return null
+  //   }
+  //   console.log(user)
+  console.log(useUser);
   const wishes = await prismadb.wishes.findMany({
     where: {
       userId,
@@ -17,11 +24,16 @@ const FetchWishes = async () => {
   return (
     <div className="overall-wish-box flex flex-col gap-[2.15rem]">
       {wishes.map((wish) => (
-        <div className="wish-box flex items-center justify-between" key={wish.id}>
+        <div
+          className="wish-box flex items-center justify-between"
+          key={wish.id}
+        >
           <div className="img-text-flex flex items-center gap-4 text-white">
             <div className="w-14 h-14 rounded-full bg-red-600"></div>
             <div>
-              <h3 className="font-[600] text-[1.15rem]">{wish.wish_description}</h3>
+              <h3 className="font-[600] text-[1.15rem]">
+                {wish.wish_description}
+              </h3>
               <p className="text-[1rem] mt-[0.15rem] text-[#9E9EB8] font-[600]">
                 {wish.wish_category}
               </p>
