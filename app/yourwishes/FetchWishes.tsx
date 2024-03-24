2ad4b6a4dde302f7e6227727fb2a30c15811b075
hayzedd2 <azeezalhameen1@gmail.com>
2024-03-24 18:28:10 +0100

    empty wish state

--- app/yourwishes/FetchWishes.tsx
+++ app/yourwishes/FetchWishes.tsx
@@ -11,42 +11,100 @@ const FetchWishes = async () => {
   if (!userId) {
     redirect("/sign-in");
   }
-  //   if(!isSignedIn){
-  //     return null
-  //   }
-  //   console.log(user)
   console.log(useUser);
   const wishes = await prismadb.wishes.findMany({
     where: {
       userId,
     },
   });
+  const wishCat = [
+    {
+      wishname: "Tech",
+      wishendpoint: "/random",
+    },
+    {
+      wishname: "Education",
+      wishendpoint: "/random",
+    },
+    {
+      wishname: "Travel",
+      wishendpoint: "/random",
+    },
+    {
+      wishname: "Health & Wellness",
+      wishendpoint: "/random",
+    },
+    {
+      wishname: "Other",
+      wishendpoint: "/random",
+    },
+  ];
 
   return (
     <div className="overall-wish-box flex flex-col gap-[2.15rem]">
+      {wishes.length <= 0 && (
+        <div className="w-full flex flex-col mt-20 items-center justify-center gap-3">
+          <h2 className="text-white text-[1.7rem]">
+            You do not have any wish created at this time.
+          </h2>
+          <Link href={"/addwish"}>
+            <Button variant={"primary"} className="text-[1.15rem] py-6 px-6">
+              Create new Wish
+            </Button>
+          </Link>
+        </div>
+      )}
       {wishes.map((wish) => (
-        < Link href={`/mywishes/${wish.wishId}`}
-          className="wish-box flex items-center justify-between"
-          key={wish.id}
-        >
-          <div className="img-text-flex flex items-center gap-4 text-white">
-            <div className="w-14 h-14 rounded-full bg-red-600"></div>
-            <div>
-              <h3 className="font-[600] text-[1.15rem]">{wish.wish_name}</h3>
-              <p className="text-[1rem] mt-[0.15rem] text-[#9E9EB8] font-[600]">
-                {wish.wish_description}
-              </p>
+        <section>
+          <div className="conatiner max-w-[75rem] mx-auto">
+            <div className="heading my-5">
+              <h1 className="text-white text-[1.7rem] font-[800]">
+                Your Wishes
+              </h1>
+              <div className="flex my-6 gap-4">
+                {wishCat.map((cat, index) => {
+                  return (
+                    <Link href={cat.wishendpoint}>
+                      <div
+                        key={index}
+                        className="pill px-5 text-[1rem] font-[500] py-2 capitalize rounded-[4px] bg-[#292939] text-white"
+                      >
+                        {cat.wishname}
+                      </div>
+                    </Link>
+                  );
+                })}
+              </div>
+            </div>
+            <div className="wish-container my-10">
+              <Link
+                href={`/mywishes/${wish.wishId}`}
+                className="wish-box flex items-center justify-between"
+                key={wish.id}
+              >
+                <div className="img-text-flex flex items-center gap-4 text-white">
+                  <div className="w-14 h-14 rounded-full bg-red-600"></div>
+                  <div>
+                    <h3 className="font-[600] text-[1.15rem]">
+                      {wish.wish_name}
+                    </h3>
+                    <p className="text-[1rem] mt-[0.15rem] text-[#9E9EB8] font-[600]">
+                      {wish.wish_description}
+                    </p>
+                  </div>
+                </div>
+                <div>
+                  <Button
+                    size={"lg"}
+                    className="text-[0.98rem] font-[600] bg-[#292939]"
+                  >
+                    Offer to help
+                  </Button>
+                </div>
+              </Link>
             </div>
           </div>
-          <div>
-            <Button
-              size={"lg"}
-              className="text-[0.98rem] font-[600] bg-[#292939]"
-            >
-              Offer to help
-            </Button>
-          </div>
-        </Link>
+        </section>
       ))}
     </div>
   );
